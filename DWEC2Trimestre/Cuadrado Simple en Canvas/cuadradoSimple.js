8fb7c27ad4c53a45cb26781e88ba3922bc9fbf6b
christiaandelrio <60599988+christiaandelrio@users.noreply.github.com>
2023-03-15 12:17:02 +0100

    Add files via upload

---
+++ DWEC2Trimestre/Cuadrado Simple en Canvas/cuadradoSimple.js
@@ -0,0 +1,7 @@
+//Definimos las variables
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+//Rellenamos el canvas
+ctx.fillStyle = 'green';
+ctx.fillRect(10,10,150,100);
